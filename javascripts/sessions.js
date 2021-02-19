ccac6961ef6293ec0f4a2d756f797c1356de290f
prathik pugazhenthi <prathik0300@gmail.com>
2021-02-20 00:14:01 +0530

    Casting Vote

---
+++ javascripts/sessions.js
@@ -0,0 +1,15 @@
+
+function set(key,value){
+    if (typeof(Storage) !== "undefined") {
+        // Store
+        sessionStorage.setItem(key, value);
+        // Retrieve
+      } else {
+        sessionStorage.setItem("ID", "None");
+      }
+}
+
+
+function show(){
+    console.log(sessionStorage);
+}
